41ecfadbd3c66cdda720238aee2d0709a5eef2c3
Oleh Kondrashov <condrashov.oleg@gmail.com>
2024-10-22 11:26:00 +0200

    registration

--- website/views.py
+++ website/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from functools import reduce
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
+def home(request):
+    #check to see if logging in
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        #Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in")
+            return redirect('home')
+        else:
+            messages.error(request, 'There was an error in logging in, please try again')
+            return redirect('home')
+
+
+    return render(request, "home.html", {})
+
+def logout_user(request):
+        logout(request)
+        messages.success(request, 'you have been logged out')
+        return redirect('home')
+
+def register_user(request):
+    return render(request, "register.html")
